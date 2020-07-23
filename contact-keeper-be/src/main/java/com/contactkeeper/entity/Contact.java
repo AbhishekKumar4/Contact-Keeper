e24741c861d3038e9c03fff4752541ace9655f2a
Abhishek <kumarabhishek0388@gmail.com>
2020-07-23 16:43:54 +0530

    adding entity

--- contact-keeper-be/src/main/java/com/contactkeeper/entity/Contact.java
+++ contact-keeper-be/src/main/java/com/contactkeeper/entity/Contact.java
@@ -1,6 +1,11 @@
 package com.contactkeeper.entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
 
 @Entity(name = "Contact")
 public class Contact {
@@ -10,6 +15,20 @@ public class Contact {
     private Long id;
 
     @Column(name = "USER_ID")
-    @ManyToOne
-    private User user;
+    private Long userId;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "CONTACT_TYPE", columnDefinition = "varchar(10) default 'primary'")
+    private String type;
+
+    @Column(name = "NAME")
+    private Date date;
 }
