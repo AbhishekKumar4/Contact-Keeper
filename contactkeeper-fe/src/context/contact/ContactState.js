b64c7791a0b622e8379f246aa5bc0cbc3ea72cc2
Abhishek <kumarabhishek0388@gmail.com>
2020-08-16 18:02:33 +0530

    updae contact and clear form

--- contactkeeper-fe/src/context/contact/ContactState.js
+++ contactkeeper-fe/src/context/contact/ContactState.js
@@ -64,7 +64,9 @@ const ContactState = props => {
     }
 
     // Update Contact
-
+    const updateContact = contact => {
+        dispatch({type: UPDATE_CONTACT, payload: contact});
+    }
     // Filter Contact
 
     // Clear Filter
@@ -75,6 +77,7 @@ const ContactState = props => {
             contacts : state.contacts,
             current: state.current,
             addContact,
+            updateContact,
             deleteContact,
             setCurrent,
             clearCurrent
