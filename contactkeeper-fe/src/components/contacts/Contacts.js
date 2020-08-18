7548bff796d7751aa29df002abee5725e63cfed2
Abhishek <kumarabhishek0388@gmail.com>
2020-08-18 22:20:52 +0530

    adding transition group

--- contactkeeper-fe/src/components/contacts/Contacts.js
+++ contactkeeper-fe/src/components/contacts/Contacts.js
@@ -1,6 +1,7 @@
-import React, { Fragment, useContext } from 'react'
-import ContactContext from '../../context/contact/contactContext'
-import ContactItem from './ContactItem'
+import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import ContactContext from '../../context/contact/contactContext';
+import ContactItem from './ContactItem';
 
 const  Contacts = () => {
 
@@ -14,13 +15,19 @@ const  Contacts = () => {
 
     return (
         <Fragment>
+            <TransitionGroup>
             { filtered !== null 
             ? filtered.map(contact => (
-            <ContactItem key = {contact.id} contact = {contact} />
+            <CSSTransition key = {contact.id} timeout = {500} classNames = "item">
+                <ContactItem contact = {contact} />
+            </CSSTransition>
             ))
             : contacts.map( contact => (
-            <ContactItem key = {contact.id} contact = {contact} />))
-            }
+            <CSSTransition key = {contact.id} timeout = {500} classNames = "item">
+                <ContactItem key = {contact.id} contact = {contact} />
+            </CSSTransition>
+            ))}
+            </TransitionGroup>
         </Fragment>
     )
 }
